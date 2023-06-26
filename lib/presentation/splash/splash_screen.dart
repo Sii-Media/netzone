64a33700c143e8b6e44033693b8bd2563b3ca0b6
WissamKahwaji <kahwajiwisam@gmail.com>
2023-06-26 19:35:41 +0400

    finish v1

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,26 +1,39 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:netzoon/presentation/core/constant/colors.dart';
 import 'package:netzoon/presentation/start_screen.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final videoPlayerController = VideoPlayerController.asset(
+      'assets/images/Final_intro_netzoon.mp4', // Update with your video file path
+    );
     return AnimatedSplashScreen(
       // splash: Lottie.asset('assets/images/Final_Intro.mp4.lottie.json'),
-      splash: Image.asset(
-        "assets/images/logo.png",
-        width: 400.w,
-        height: 400.h,
-        fit: BoxFit.cover,
+      // splash: Image.asset(
+      //   "assets/images/logo.png",
+      //   width: 400.w,
+      //   height: 400.h,
+      //   fit: BoxFit.cover,
+      // ),
+      splash: Chewie(
+        controller: ChewieController(
+            videoPlayerController: videoPlayerController,
+            autoPlay: true,
+            looping: false,
+            showControlsOnInitialize: false,
+            showControls: false),
       ),
-      backgroundColor: AppColor.backgroundColor,
-      splashTransition: SplashTransition.fadeTransition,
-      animationDuration: const Duration(seconds: 1),
-      splashIconSize: 450,
+      backgroundColor: Color.fromARGB(255, 92, 121, 164),
+      // splashTransition: SplashTransition.fadeTransition,
+      animationDuration: const Duration(seconds: 3),
+      splashIconSize: 500.h,
       // duration: 1000,
       nextScreen: const StartScreen(),
     );
